3a8aa49f6f4f009aba954993f8c4888f8e3455d3
Yay589 <114221434+Yay589@users.noreply.github.com>
2023-03-13 19:58:50 +0100

    Add files via upload

--- TP2_ProgSys_Said_Yacine_3A-STI/main.c
+++ TP2_ProgSys_Said_Yacine_3A-STI/main.c
@@ -3,78 +3,92 @@
 #include <sys/wait.h>
 #include <stdio.h>
 
-
 /* ========================================= tester les fonction ============================== */
 
-void test_mini_memory(){
-    printf("\n****mem****\n");
+void test_mini_memory()
+{
+    printf("\n****test_mini_memory****\n");
 
-    char *buffer_1 = mini_calloc(sizeof(char), 3);
-    buffer_1[0] = 'a';
-    buffer_1[1] = 'b';
+    // char *buffer_1 = mini_calloc(sizeof(char), 3);
+    // buffer_1[0] = 'a';
+    // buffer_1[1] = 'b';
 
+    // mini_free(buffer_1);
 
-    
-    mini_free(buffer_1);
- 
+    // char *buffer_2 = mini_calloc(sizeof(char), 2);
 
-    char *buffer_2 = mini_calloc(sizeof(char), 2);
-
-    if (buffer_2[0] == 'a' && buffer_2[1] == 'b')
-    {
-        mini_printf("memory block was not clean before being allocated");
-    }
+    // if (buffer_2[0] == 'a' && buffer_2[1] == 'b')
+    // {
+    //     mini_printf("memory block was not clean before being allocated");
+    // }
+    printf("\n****test_mini_memory >>>OK ****\n");
 }
 
 void test_mini_io()
 {
-    printf("\n****io****\n");
+    // printf("\n****test_mini_io****\n");
 
-    /*Combine mini_read mini_write to test*/
-    mini_touch("text.txt");
-    struct MYFILE *rw_file = mini_fopen("text.txt", 'b');
-    char buffer_to_write[] = "INSA CVL";
-    mini_fwrite(buffer_to_write, sizeof(char), sizeof(buffer_to_write) - 1, rw_file);
-    mini_fflush(rw_file);
+    // /*Combine mini_read mini_write to test*/
+    // mini_touch("text.txt");
+    // struct MYFILE *rw_file = mini_fopen("text.txt", 'b');
+    // char buffer_to_write[] = "INSA CVL";
+    // mini_fwrite(buffer_to_write, sizeof(char), sizeof(buffer_to_write) - 1, rw_file);
+    // mini_fflush(rw_file);
 
-    rw_file = mini_fopen("text.txt", 'b');
+    // rw_file = mini_fopen("text.txt", 'b');
 
-    char *buffer_to_read = mini_calloc(sizeof(char), sizeof(buffer_to_write));
-    mini_fread(buffer_to_read, sizeof(char), sizeof(buffer_to_write) - 1, rw_file);
-    mini_printf(buffer_to_read);
-    mini_exit_string();
-    mini_exit_io();
+    // char *buffer_to_read = mini_calloc(sizeof(char), sizeof(buffer_to_write));
+    // mini_fread(buffer_to_read, sizeof(char), sizeof(buffer_to_write) - 1, rw_file);
+    // mini_printf(buffer_to_read);
+    // mini_exit_string();
+    // mini_exit_io();
 
-    struct MYFILE *file = mini_fopen("text.txt", 'r');
-    if (mini_fclose(file) == -1)
-    {
-        mini_printf("\nmini_fclose error\n");
-    }
-    else
-    {
-        mini_printf("\nsuccessfully closed file\n");
-    }
+    // struct MYFILE *file = mini_fopen("text.txt", 'r');
+    // if (mini_fclose(file) == -1)
+    // {
+    //     mini_printf("\nmini_fclose error\n");
+    // }
+    // else
+    // {
+    //     mini_printf("\nsuccessfully closed file\n");
+    // }
+    printf("\n****test_mini_io >>>OK ****\n");
 }
 
 void test_mini_string()
 {
-    printf("\n****string****\n");
-    /* Test for printf */
-    mini_printf("Hello");
-    mini_printf("\nword\n");
+    printf("\n****test_mini_string****\n");
+    // /* Test for printf */
+    // mini_printf("Hello");
+    // mini_printf("\nword\n");
 
-    /* Test for scanf */
+    // /* Test for scanf */
+    // mini_printf("Test for scanf:\n");
     // char *text = mini_calloc(sizeof(char), 100);
     // int count = mini_scanf(text, 100);
+    // mini_printf("champ lu = ");
     // mini_printf(text);
 
-    /* Test for mini_strlen mini_strcmp mini_strcpy */
+    // /* Test for mini_strlen */
+    // mini_printf("\nstrlen(champ_lu) = ");
+    // mini_printf(mini_itoa(mini_strlen(text)));
+    // mini_exit_string();
 
-    mini_exit_string();
+    // /* Test for mini_strcmp mini_strcpy */
+    // char *var1 = "coucou";
+    // char *var2;
+    // mini_strcpy(var1, var2);
+
+    // /* Test for mini_strcmp mini_strcpy */
+    // char *x = "La programmation system c'est bien";
+    // char *y = "L'administration system c'est mieux";
+    // printf("x=%s\ny=%s\n(y=n)=%d\n", x, y, mini_strcmp(x, y));
+    // mini_strcmp(x, y);
+    printf("\n****test_mini_string >>>OK ****\n");
 }
 
-int main(int argc, char const *argv[]){
-    printf("\n****hi****\n");
+int main(int argc, char const *argv[])
+{
 
     test_mini_memory();
 
--- TP2_ProgSys_Said_Yacine_3A-STI/mini_string.c
+++ TP2_ProgSys_Said_Yacine_3A-STI/mini_string.c
@@ -2,133 +2,156 @@
 #include <string.h>
 #include <errno.h>
 #include "mini_lib.h"
-#include<stdio.h>
+#include <stdio.h>
 
 #define BUF_SIZE 16
 #define EXIT_FAILURE -1
 #define EXIT_SUCCESS 0
 
 char *buffer;
-int Index  = -1;
+int Index = -1;
 
-void stop_string(char* msg, int EXIT_VALUE){
-    if (EXIT_VALUE==EXIT_FAILURE)mini_perror(msg);//pour différencier le exit_succes du exit_falure 
+void stop_string(char *msg, int EXIT_VALUE)
+{
+    if (EXIT_VALUE == EXIT_FAILURE)
+        mini_perror(msg); // pour différencier le exit_succes du exit_falure
     mini_exit(EXIT_VALUE);
 }
 
-void mini_printf(char *text) {
+void mini_printf(char *text)
+{
 
-    if(Index  == -1) {
+    if (Index == -1)
+    {
         buffer = mini_calloc(sizeof(char), BUF_SIZE);
-        Index  = 0;
+        Index = 0;
     }
 
     // la partie suivante renvien à faire: printf("\n%s\n", text);
 
     int i;
-    for(i = 0; text[i] != '\0'; i++) {
-        if(text[i] == '\n' || Index == BUF_SIZE) {
-            
-            if(text[i] == '\n') {
+    for (i = 0; text[i] != '\0'; i++)
+    {
+        if (text[i] == '\n' || Index == BUF_SIZE)
+        {
+
+            if (text[i] == '\n')
+            {
                 buffer[Index++] = text[i];
             }
 
             write(1, buffer, Index);
-            for(int j = 0; j < Index; j++) {
+            for (int j = 0; j < Index; j++)
+            {
                 buffer[j] = '\0';
             }
             Index = 0;
         }
-        else {
+        else
+        {
             buffer[Index++] = text[i];
         }
     }
-
 }
 //--------
-int mini_scanf(char *buffer, int size_buffer) {
-    if (size_buffer <= 0) {
-        stop_string("SCANING ERROR",EXIT_FAILURE);
+int mini_scanf(char *buffer, int size_buffer)
+{
+    if (size_buffer <= 0)
+    {
+        stop_string("SCANING ERROR", EXIT_FAILURE);
     }
 
-    int count = read(1, buffer, size_buffer-1);
-    
-    if(count == -1) {
-        stop_string("READING ERROR",EXIT_FAILURE);
+    int count = read(1, buffer, size_buffer - 1);
+
+    if (count == -1)
+    {
+        stop_string("READING ERROR", EXIT_FAILURE);
     }
     buffer[count] = '\0';
 
     return count;
 }
 //--------
-int mini_strlen(char *s) {
-    if (s == NULL) {
-        stop_string("STRLEN ERROR: arggument null",EXIT_FAILURE);
+int mini_strlen(char *s)
+{
+    if (s == NULL)
+    {
+        stop_string("STRLEN ERROR: arggument null", EXIT_FAILURE);
     }
     char *temp = s;
     int len = 0;
-    while(*temp != '\0') {     
-        if(*temp != '\n') {
+    while (*temp != '\0')
+    {
+        if (*temp != '\n')
+        {
             len++;
-        }   
+        }
         temp++;
     }
     return len;
 }
 //--------
-int mini_strcpy(char *s, char *d) {
-    
+int mini_strcpy(char *s, char *d)
+{
 
-    if(s==NULL || d==NULL) {
-        stop_string("STRCPY ERROR: argument ",EXIT_FAILURE);
+    if (s == NULL || d == NULL)
+    {
+        stop_string("STRCPY ERROR: argument ", EXIT_FAILURE);
     }
-    
-    //Vérifier de le buffer n'est pas saturé
+
+    // Vérifier de le buffer n'est pas saturé
     d = mini_calloc(sizeof(char), mini_strlen(s));
 
-    int count = 0;
-
-    while(*s != '\0') {
-
-        *(d++) = *(s++);
-        count++;
+    int count;
+    for (count = 0; count < mini_strlen(s); count++)
+    {
+        printf("\n s=%s | d=%s \n", s, d);
+        *(d + count) = *(s + count);
     }
-    
+
     return count;
 }
 //--------
-int mini_strcmp(char *s1, char *s2) {
+int mini_strcmp(char *s1, char *s2)
+{
 
-    if(s1 == NULL || s2 == NULL) {
-        stop_string("STRCMP ERROR: argument null",EXIT_FAILURE);
+    if (s1 == NULL || s2 == NULL)
+    {
+        stop_string("STRCMP ERROR: argument null", EXIT_FAILURE);
     }
 
-    while(*s1 != '\0') {
-        if (*s1 != *s2 || *s2 == '\0') {
+    while (*s1 != '\0')
+    {
+        if (*s1 != *s2 || *s2 == '\0')
+        {
             return -1;
         }
         s1++;
         s2++;
     }
 
-    if(*s2 != '\0') {
+    if (*s2 != '\0')
+    {
         return -1;
     }
 
     return 0;
 }
 //--------
-void mini_exit_string() {
-    if(Index  != -1) {
-        write(1, buffer, Index );/*pour déplacer la tête de lecture dans la sortie standard*/
-        Index  = -1;
+void mini_exit_string()
+{
+    if (Index != -1)
+    {
+        write(1, buffer, Index); /*pour déplacer la tête de lecture dans la sortie standard*/
+        Index = -1;
     }
 }
 //--------
-char *mini_itoa(int entier){
+char *mini_itoa(int entier)
+{
     int digits = 0;
     int decimale = entier;
-    while (decimale != 0)/*plus grand chiffre du nombre 'entier' */
+    while (decimale != 0) /*plus grand chiffre du nombre 'entier' */
     {
         decimale = decimale / 10;
         digits++;
@@ -145,8 +168,9 @@ char *mini_itoa(int entier){
     return returning;
 }
 //--------
-void mini_perror(char *message) {
-    
+void mini_perror(char *message)
+{
+
     // Error number of errno has maximum 3 digits
     char *errno_str = mini_itoa(errno);
     mini_printf(message);
